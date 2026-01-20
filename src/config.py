c45827eb77d64213f875a347a7fe4d52868440c0
SONG-36 <andysong1379@gmail.com>
2026-01-20 09:08:29 +0800

    Define final clean data schema and constants

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# src/config.py
+
+STANDARD_COLUMNS = [
+    "date",
+    "description",
+    "amount",
+    "currency",
+    "notes",
+    "source_file",
+]
+
+DEFAULT_CURRENCY = "UNKNOWN"
+
+DATE_OUTPUT_FORMAT = "%Y-%m-%d"
+
+SUMMARY_KEYWORDS = [
+    "subtotal",
+    "total",
+    "opening balance",
+]
